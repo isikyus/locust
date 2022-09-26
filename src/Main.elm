4a9ff017ef7c989e894eafccbef7865863c9ab1b
Isikyus <isikyus@users.noreply.github.com>
2022-09-26 22:50:37 +1000

    Get a grid of cells rendering
    This is a good step towards cellular automata, but it may be a dead end.
    What I want to do next is define a block cellular automaton, but that
    requires operations on the grid that are only well-defined if it's of
    even size, and the type system of Elm isn't strong enough to make or
    understand those guarantees.
    
    I'm going to have a look at other languages and see if there's something
    else that can express this better.

--- src/Main.elm
+++ src/Main.elm
@@ -3,9 +3,20 @@ module Main exposing (main)
 import Browser
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+import Grid
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+  Browser.sandbox
+    { init = init
+    , update = update
+    , view = view
+    }
+
+init =
+  Grid.initialize
+    gridWidth
+    gridHeight
+    (+)
 
 type Msg = Update
 
@@ -14,11 +25,22 @@ update msg model =
     Update ->
       model
 
+gridWidth
+  = 40
+
+gridHeight
+  = 40
+
+maxValue
+  = gridWidth + gridHeight
+
+cellSize = 10
+
 imgWidth
-  = 400
+  = gridWidth * cellSize
 
 imgHeight
-  = 400
+  = gridHeight * cellSize
 
 view model =
   svg
@@ -26,10 +48,30 @@ view model =
     , height ( String.fromFloat imgHeight )
     , viewBox "0 0 400 400"
     ]
-    [ text_
-      [ x ( String.fromFloat (imgWidth / 2) )
-      , y ( String.fromFloat (imgHeight / 2) )
-      , fill "green"
-      ]
-      [ text "Locust has Landed" ]
-    ]
+    ( Grid.foldl
+        (::)
+        []
+        ( Grid.indexedMap
+            ( \gX gY val ->
+              rect
+                [ x ( String.fromInt ( gX * cellSize ) )
+                , y ( String.fromInt ( gY * cellSize ) )
+                , width ( String.fromInt cellSize )
+                , height ( String.fromInt cellSize )
+                , fillOpacity
+                    ( String.fromFloat
+                      ( (toFloat val) / (toFloat maxValue) )
+                    )
+                , fill "green"
+                ]
+                []
+            )
+            model
+        )
+    )
+--    , text_
+--      [ x ( String.fromFloat (imgWidth / 2) )
+--      , y ( String.fromFloat (imgHeight / 2) )
+--      , fill "green"
+--      ]
+--      [ text "Locust has Landed" ]
